4e357a27dc5156cea1c3e2b90d3b2dc0c3a7cb4e
robbie.vanbrabant <robbie.vanbrabant@222134ff-5340-0410-a1f4-a3a7d1acb896>
2008-04-19 15:46:18 +0000

    trying out how customization would work

---
+++ BuilderBuilder/src/org/garbagecollected/util/SimpleSetterBuilderSpecification.java
@@ -0,0 +1,34 @@
+package org.garbagecollected.util;
+
+import java.lang.reflect.Method;
+
+/**
+ * Use setters for writers, and simple names for readers.
+ * @author Robbie Vanbrabant
+ */
+public class SimpleSetterBuilderSpecification implements BuilderSpecification {
+  private final Class<?> spec;
+  
+  public SimpleSetterBuilderSpecification(Class<?> spec) {
+    this.spec = spec;
+  }
+  
+  public boolean isWriter(Method method, Object[] args) {
+    return (args != null && args.length == 1 
+            && spec.isAssignableFrom(method.getReturnType()));
+  }
+
+  public boolean isReader(Method method, Object[] args) {
+    return args == null
+        && method.getName().startsWith("get")
+        && !Void.TYPE.isAssignableFrom(method.getReturnType());
+  }
+
+  public String readerIdentity(Method reader) {
+    return reader.getName().substring(3, reader.getName().length());
+  }
+
+  public String writerIdentity(Method writer) {
+    return writer.getName();
+  }
+}
