b6e3006fba019c6cc53e1d75d2b38e14be12f7c1
robbie.vanbrabant <robbie.vanbrabant@222134ff-5340-0410-a1f4-a3a7d1acb896>
2008-04-19 23:12:27 +0000

    benchmark with google charts

--- trunk/BuilderBuilder/src/org/garbagecollected/util/Benchmark.java
+++ trunk/BuilderBuilder/src/org/garbagecollected/util/Benchmark.java
@@ -1,12 +1,21 @@
 package org.garbagecollected.util;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
 import org.garbagecollected.util.Example.ManualBuilder;
 
+import com.googlecode.gchartjava.AxisInfo;
+import com.googlecode.gchartjava.Color;
+import com.googlecode.gchartjava.Data;
+import com.googlecode.gchartjava.Line;
+import com.googlecode.gchartjava.LineChart;
+
 /** Comparing performance to regular builders. */
 public class Benchmark {
-  private static void proxy(int count) {
+  private static long proxy(int count) {
     long start = System.currentTimeMillis();
     int orig = count;
     while (count-->0) {
@@ -18,11 +27,13 @@ public class Benchmark {
       .toString();
     }
     
+    long elapsed = System.currentTimeMillis() - start;
     System.out.printf("%10s%,13d%8d%1s%n", "Proxy", orig, 
-                      System.currentTimeMillis() - start, "ms");
+                      elapsed, "ms");
+    return elapsed;
   }
 
-  private static void noProxy(int count) {
+  private static long noProxy(int count) {
     long start = System.currentTimeMillis();
     int orig = count;
     while (count-->0) {
@@ -32,15 +43,62 @@ public class Benchmark {
       .build()
       .toString();
     }
-
+    
+    long elapsed = System.currentTimeMillis() - start;
     System.out.printf("%10s%,13d%8d%1s%n", "Manual", orig, 
-                      System.currentTimeMillis() - start, "ms");
+                      elapsed, "ms");
+    return elapsed;
   }
   
   public static void main(String[] args) {
-    for (int n : Arrays.asList(10, 50, 100, 500, 1000)) {
-      proxy(n * 1000);
-      noProxy(n * 1000);
-    }    
+    List<Integer> multipliers = Arrays.asList(3, 5, 10, 20, 40, 80, 150);
+    float[] proxyX = new float[multipliers.size()];
+    float[] noProxyX = new float[multipliers.size()];
+    float totalProxy = 0F;
+    float totalNoProxy = 0F;
+    int counter = 0;
+    
+    for (int n : multipliers) {
+      int number = n * 1000;
+      proxyX[counter] = (float)proxy(number);
+      totalProxy+=proxyX[counter];
+      noProxyX[counter++] = (float)noProxy(number);
+      totalNoProxy+= noProxyX[counter-1];
+    }
+    totalNoProxy = 5000;
+    totalProxy = 5000;
+    Data proxyXData = new Data(toPercentages(proxyX, totalProxy));
+    Data noProxyXData = new Data(toPercentages(noProxyX, totalNoProxy));
+    
+    Line proxyLine = new Line(proxyXData);
+    proxyLine.setLegend("Proxy Builder");
+    proxyLine.setColor(Color.RED);
+    
+    Line noProxyLine = new Line(noProxyXData);
+    noProxyLine.setLegend("Manual Builder");
+    noProxyLine.setColor(Color.BLACK);
+    
+    LineChart chart = new LineChart(proxyLine, noProxyLine);
+    
+    chart.addYAxisInfo(new AxisInfo("","1 sec","2 sec","3 sec", "4 sec", "5 sec"));
+    
+    List<String> xLabels = new LinkedList<String>();
+    for(Integer i : multipliers) {
+      xLabels.add(String.format(Locale.US, "%,d", i));
+    }
+    chart.addXAxisInfo(new AxisInfo(xLabels));
+    chart.addXAxisInfo(new AxisInfo("","","","","","","Objects Created"));
+    
+    chart.setTitle("BuilderProxy Benchmark on JDK 6");
+    chart.setSize(400, 300);
+    System.out.println(chart.createURLString());
+    
+  }
+  
+  static float[] toPercentages(float[] values, float total) {
+    for (int i = 0; i < values.length; i++) {
+      values[i] = (int)(values[i]/(total/100));
+    }
+    return values;
   }
 }
